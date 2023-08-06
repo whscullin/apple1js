418c714c453a181f9e7e99b195f320b84351fd48
Will Scullin <scullin@scullinsteel.com>
2023-08-05 18:27:57 -0700

    Convert RAM to Typescript

---
+++ js/types.ts
@@ -0,0 +1,16 @@
+/* Copyright 2023 Will Scullin <scullin@scullinsteel.com>
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation.  No representations are made about the suitability of this
+ * software for any purpose.  It is provided "as is" without express or
+ * implied warranty.
+ */
+
+export type byte = number;
+
+export type word = number;
+
+export type address = word;
